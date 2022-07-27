6a4ca1b7ff79a77636a30bfae554b42a3f152ed6
CarolinaVallejos <carolina.vallejos182@gmail.com>
2022-07-27 20:32:21 -0300

    js1

---
+++ js/main.js
@@ -0,0 +1,75 @@
+/*let num1 = 12
+let num2 = 21
+alert( num1 + num2)
+console.log( num1 + num2)
+ 
+
+
+let nombreIngresado   = prompt("Ingresar nombre");
+
+if((nombreIngresado == "CARO") || (nombreIngresado =="caro")  || (nombreIngresado == "Caro")){
+    alert("El nombre ingresado es Caro"); 
+}else{
+    alert("Error: El nombre ingresado NO ES Caro"); 
+}
+*/
+
+
+
+// Actividad 1
+
+/*
+let edad = prompt("Ingresar edad");
+//Repetir con While hasta  "ESC"
+while(edad != "ESC" ){
+    alert(" Edad Ingresada "+ edad);
+    
+    edad = prompt("Ingresar edad nuevamente");
+}
+
+
+//Actividad 2:
+
+for ( let i = 1; i <= 25; i++) {
+// en cada repetición se pide nombre y apellido de alumno
+let IngresarNombre = prompt ("Ingresar Nombre");
+let IngresarApellido = prompt ( "Ingresar Apellido");
+// Ingresar Asistencia de Clases usando numero de repetición
+alert ("Asistencia N°:" + " " +i+" "+ IngresarNombre+" "+ IngresarApellido );
+}
+
+
+
+// Actividad 3: Switch
+
+let usuario = prompt(" Ingrese usuario");
+while(usuario != "ESC" ){
+switch (usuario) {
+    case "profesores":
+        alert("Hola Profesores");
+        break;
+    case "directores":
+        alert("Hola Directores");
+        break;
+    default:
+        alert("Error Usuario");
+        break;
+    }
+    usuario = prompt("Ingresar un usuario válido");
+}
+*/
+
+ // Actividad 4: 
+ let numero = 20
+ if ( numero <= 20){
+    alert("HOLA!");
+ } else {
+    alert("Ingresar otro número")
+ }
+
+
+
+
+
+
+
