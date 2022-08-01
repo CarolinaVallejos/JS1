c818cd41b0b00459da99c0bebfa6ee954d34d0db
CarolinaVallejos <carolina.vallejos182@gmail.com>
2022-08-01 17:18:05 -0300

    funciones

--- js/main.js
+++ js/main.js
@@ -57,15 +57,82 @@ switch (usuario) {
     }
     usuario = prompt("Ingresar un usuario válido");
 }
+
+
+ 
+
+
+
+// Actividad Simulador 1. Ingreso de horas.
+
+
+
+    let cantidad1 = parseInt(prompt("Ingresar Cantidad de horas trabajadas"));
+    alert("La cantidad de horas trabajadas son = " + cantidad1)
+    let horas = 1050
+    
+    function retorno (){return "cantidad1"}
+    const retornoFuncion = retorno()
+    
+    function horasT () { return "horas"}
+    const funcionretorno = horasT ()
+
+    console.log("La cantidad de hs trabajadas son: "+ cantidad1)
+    console.log("Pago por hora:" +horas)
+    
+    
+    ////  mutiplicacion
+    let resultado = cantidad1 * horas
+    console.log ("El monto a pagar es de : " + resultado)
+
+
 */
 
- // Actividad 4: 
- let numero = 20
- if ( numero <= 20){
-    alert("HOLA!");
- } else {
-    alert("Ingresar otro número")
- }
+// Actividad Simulador 2
+
+// ingresar préstamo
+
+function cantidad1(){
+    let cantidad1 = parseInt(prompt("Ingresar Monto del Préstamo"));
+    alert("La cantidad del préstamo es = " + cantidad1)
+    return cantidad1
+}
+
+//Funcion para ingresar cuotas a pagar
+function cuotas1(){
+    let cuotas1 = parseInt(prompt("En cuantas cuotas desea abonar el préstamo ? Elija de 1 a 12"));
+    alert("Desea pagar el préstamo en " + cuotas1 + " cuotas?")
+    return cuotas1
+}
+
+//Resultado de funciones
+let cantidad = cantidad1()
+let cuotas = cuotas1()
+
+//Muestro resultado
+console.log("La cantidad total del préstamo es: " + cantidad)
+console.log("Las cuotas son:" + cuotas)
+
+//Divido la cant de cuotas
+function dividir(dato1, dato2){
+   let resultado = dato1 / dato2;
+   return resultado
+}
+
+
+let division = dividir(cantidad, cuotas)
+
+//Resultado
+console.log("Usted debe pagar " + cuotas + " cuotas de " + division)
+    
+    
+
+   
+
+    
+
+
+
 
 
 
