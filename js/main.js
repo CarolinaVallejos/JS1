333f6e86dc2a9e1bde5959afac319b5d82794bdc
CarolinaVallejos <carolina.vallejos182@gmail.com>
2022-08-19 18:25:32 -0300

    js dom evento

--- js/main.js
+++ js/main.js
@@ -1,118 +1,87 @@
-
-
-
-// Actividad Simulador 1. Ingreso de horas.
-
+// Array
 /*
 
-let HorasTrabajadas = parseInt(prompt("Ingresar Cantidad de horas trabajadas"));
-alert("La cantidad de horas trabajadas son = " + HorasTrabajadas);
-while (HorasTrabajadas != "ESC") {
+let nombreUsuario = prompt ("Ingresar Nombre");
+alert ("El nombre Ingresado es :"+ " " +nombreUsuario)
 
-    const horas = 1050
+if (nombreUsuario.length == 0 ) { 
+console.log("Ingreso Invalido")
+}else{ 
+    console.log (nombreUsuario)
+ }
 
-    function retorno() { return "HorasTrabajadas" }
-    const retornoFuncion = retorno()
-
-    function horasT() { return "horas" }
-    const funcionretorno = horasT()
-
-    console.log("La cantidad de hs trabajadas son: " + HorasTrabajadas)
-    console.log("Pago por hora:" + horas)
-
-
-    ////  mutiplicacion
-    const resultado = HorasTrabajadas * horas
-    console.log("El monto a pagar es de : " + resultado);
-    HorasTrabajadas = prompt ("Ingresar Nueva Cantidad de horas Trabajadas");
-}
-
-
-
-// Actividad Simulador 2
-
-// ingresar préstamo
-
-function MontoPrestamo(){
-    let MontoPrestamo = parseInt(prompt("Ingresar Monto del Préstamo"));
-    alert("La cantidad del préstamo es = " + MontoPrestamo)
-    return MontoPrestamo
-}
-
-//Funcion para ingresar cuotas a pagar
-function IngresarCuotas(){
-    let IngresarCuotas = parseInt(prompt("En cuantas cuotas desea abonar el préstamo ? Elija de 1 a 12"));
-    alert("Desea pagar el préstamo en " + IngresarCuotas + " cuotas?")
-    return IngresarCuotas
-}
-
-//Resultado de funciones
-let cantidad = MontoPrestamo()
-let cuotas = IngresarCuotas()
-
-//Muestro resultado
-console.log("La cantidad total del préstamo es: " + cantidad)
-console.log("Las cuotas son:" + cuotas)
-
-//Divido la cant de cuotas
-function dividir(dato1, dato2){
-   let resultado = dato1 / dato2;
-   return resultado
-}
-
-
-let division = dividir(cantidad, cuotas)
-
-//Resultado
-console.log("Usted debe pagar " + cuotas + " cuotas de " + division)
-
-*/
-
-
-// Array
-
-
-let NombreUsuario = prompt ("Ingresar Nombre");
-alert ("El nombre Ingresado es :"+ " " +NombreUsuario)
-
-let HorasTrabajadas = parseInt(prompt("Ingresar Horas"));
+let horasTrabajadas = parseInt(prompt("Ingresar Horas"));
 alert("Evaluando Horas Ingresadas");
 
  //Reduce
-const RegistroHoras = [8,10,6,9,12,15,7]
-RegistroHoras.push(HorasTrabajadas)
-console.log(RegistroHoras.length)
-console.log (RegistroHoras)
+const registroHoras = [8,10,6,9,12,15,7]
+registroHoras.push(horasTrabajadas)
+console.log(registroHoras.length)
+console.log (registroHoras)
  
 
 
-const totalHoras = RegistroHoras.reduce((acumulador, elemento) => acumulador + elemento, 0)
+const totalHoras = registroHoras.reduce((acumulador, elemento) => acumulador + elemento, 0)
 
 console.log(totalHoras)
 
 
 // Verificación
 
-while (HorasTrabajadas < 0 || isNaN(HorasTrabajadas)) {
+while (horasTrabajadas < 0 || isNaN(horasTrabajadas)) {
     alert ("Valor Inválido");
-    } HorasTrabajadas = prompt ("Ingrese un nuevo valor");
+    } horasTrabajadas = prompt ("Ingrese un nuevo valor");
+    
+    registroHoras.push (horasTrabajadas)
      
 
 
 //MIN MAX- Horas
-console.log( Math.max(...RegistroHoras ) )   
+console.log( Math.max(...registroHoras ) )   
  
 //SORT
-const ItemsHoras = [...RegistroHoras];
-ItemsHoras.sort(function(a, b) {
+const itemsHoras = [...registroHoras];
+itemsHoras.sort(function(a, b) {
     return a - b;
   });
-  console.log(ItemsHoras);
+  console.log(itemsHoras);
 
 //Date
 console.log( new Date() )
 
 
+// DOM
+
+let contenedor = document.getElementsByTagName("div");
+console.log(contenedor[0].innerHTML);
+console.log(contenedor[1].innerHTML);
+console.log(contenedor[2].innerHTML);
+
+let principal = document.getElementById("principal")
+console.log (principal.innerHTML)
+// MODIFICO
+principal.innerHTML = "Gestión De Tiempo"
+console.log (principal.innerHTML)
+*/
+//EVENTO
 
 
+let boton = document.getElementById("opcion");
+boton.onclick = () => { console.log("Respuesta") };
+boton.onmousemove = () => { console.log("Mover") }
+
+// evento teclado y boton submit
+
+let validacion = document.getElementById("formulario");
+validacion.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+  e.preventDefault();
+  console.log("Formulario Entregado");
+}
+
+let campo  = document.getElementById("name")
+campo.addEventListener("input", () => {
+     console.log(campo.value)
+})
 
